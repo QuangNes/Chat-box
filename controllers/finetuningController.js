bb2a243360fe725a4a98ce77313453dba6e6125f
Tu <thanhtuhr01@gmail.com>
2024-03-14 17:13:16 +0700

    modified some functioname. Fixed potential error when change finetune model name. Added ChatOpenAI and OpenAIEmbedings fromm langchain to for later usageg

--- controllers/finetuningController.js
+++ controllers/finetuningController.js
@@ -1,22 +1,23 @@
 const fs = require('fs')
 const path = require('path')
-const { initMessages, openai, getModelId } = require("../configs/openai")
+const { getInitMessages: initMessages, openai, getFineTuningModelName } = require("../utils/openai")
+const botList = require('../configs/botList')
 
 const getInitMessages = (_, res) => {
-    res.status(200).json(initMessages)
+    res.status(200).json(initMessages(botList[0]))
 }
 
 const reply = async (req, res) => {
     const completion = await openai.chat.completions.create({
         messages: req.body,
-        model: getModelId()
+        model: getFineTuningModelName()
     });
 
     res.status(200).json({content: completion.choices[0].message.content, role:'assistant'})
 }
 
 const saveFeedback = (req, res) => {
-    fs.writeFileSync(path.join(__dirname, '../data/feedbacks.jsonl'), JSON.stringify(req.body) + "\n", {flag:"a"})
+    fs.writeFile(path.join(__dirname, '../data/feedbacks.jsonl'), JSON.stringify(req.body) + "\n", {flag:"a"})
     res.status(200)
 }
 
---
+++ utils/openai.js
@@ -0,0 +1,61 @@
+const {OpenAI} = require('openai')
+const fs = require("fs");
+const path = require('path');
+const { OpenAIEmbeddings, ChatOpenAI } = require('@langchain/openai');
+require('dotenv').config()
+
+const FINE_TUNING__MODEL_PATH = path.join(__dirname, '../configs/fine_tuning_model.txt');
+
+const openai = new OpenAI({apiKey:process.env.OPENAI_API_KEY});
+
+const openAIEmbeding = new OpenAIEmbeddings({
+  openAIApiKey: process.env.OPENAI_API_KEY,
+  modelName: "text-embedding-3-large"
+})
+
+const getInitMessages = (bot) => {
+  return {
+    messages: [
+        {
+          role: "system",
+          content: `${bot.name}: ${bot.description}`
+        },
+        {
+          content: `Xin chào, tôi là ${bot.name}, tôi có thể giúp gì cho bạn!`,
+          role: "assistant"
+        }
+    ]
+  }
+}
+
+const getFineTuningModelName = () => {
+  return fs.readFileSync(FINE_TUNING__MODEL_PATH, "utf8");
+}
+
+const updateFineTuningModelName = (model_id) => {
+  fs.writeFile(FINE_TUNING__MODEL_PATH, model_id);
+}
+
+const chatOpenAI = new ChatOpenAI({
+  openAIApiKey: process.env.OPENAI_API_KEY,
+  modelName: getFineTuningModelName()
+})
+
+const getChatOpenAI = () => {
+  if (chatOpenAI.modelName !== getFineTuningModelName()){
+    chatOpenAI = new ChatOpenAI({
+      openAIApiKey: process.env.OPENAI_API_KEY,
+      modelName: getFineTuningModelName()
+    })
+  }
+  return chatOpenAI;
+}
+
+module.exports = {
+    openai,
+    openAIEmbeding,
+    getInitMessages,
+    getFineTuningModelName,
+    updateFineTuningModelName,
+    getChatOpenAI
+}
